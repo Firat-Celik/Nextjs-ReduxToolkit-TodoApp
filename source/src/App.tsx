f507e4eeb97d5c055e2b3384a68cc75e353c9aa1
Firat-Celik <56916843+Firat-Celik@users.noreply.github.com>
2023-03-09 07:27:31 +0300

    .

---
+++ source/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Todo from './pages/todo/Todo'
+function App() {
+  return (
+    <div className="App mt-5">
+      <Todo />
+    </div>
+  );
+}
+export default App;
