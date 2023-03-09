fd23238acb8d340e5f8c308b5428652c93881f6c
Firat-Celik <56916843+Firat-Celik@users.noreply.github.com>
2023-03-09 07:26:35 +0300

    .

---
+++ source/src/pages/todo/Todo.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "../todo/Todo.css";
+
+function Todo() {
+  return (
+    <div>
+      <section className="todoapp">
+        <header className="header mt-5">
+          <h1>Yapılacaklar</h1>
+          <form>
+            <input
+              className="new-todo"
+              placeholder="What needs to be done?"
+              autoFocus
+            />
+          </form>
+        </header>
+
+        <section className="main">
+          <input className="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">Tamamlananlar</label>
+
+          <ul className="todo-list">
+            <li className="completed">
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Learn JavaScript</label>
+                <button className="destroy"></button>
+              </div>
+            </li>
+            <li>
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Learn React</label>
+                <button className="destroy"></button>
+              </div>
+            </li>
+            <li>
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Have a life!</label>
+                <button className="destroy"></button>
+              </div>
+            </li>
+          </ul>
+        </section>
+
+        <footer className="footer">
+          <span className="todo-count">
+            <strong>2</strong>
+            items left
+          </span>
+
+          <ul className="filters">
+            <li>
+              <a href="#/" className="selected">
+                All
+              </a>
+            </li>
+            <li>
+              <a href="#/">Active</a>
+            </li>
+            <li>
+              <a href="#/">Completed</a>
+            </li>
+          </ul>
+
+          <button className="clear-completed">Clear completed</button>
+        </footer>
+      </section>
+    </div>
+  );
+}
+
+export default Todo;
